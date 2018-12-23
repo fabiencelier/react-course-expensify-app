a3a14d55c738f8393814cee8f01fcf9656f7606f
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-23 17:44:31 +0100

    Section 9, Lecture 77

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route }from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boile plate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is Dashboard</div>
+);
+
+const AddExpensePage = () => (
+  <div>This is Add page</div>
+);
+
+const EditExpensePage = () => (
+  <div>This is Edit page</div>
+);
+
+const HelpPage = () => (
+  <div>This is Help page</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' exact={true} component={ExpenseDashboardPage} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
