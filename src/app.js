928812e7f2733ef9875e50f3eb1c381e841f1044
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-23 18:03:26 +0100

    Section 9, Lecture 79

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch }from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch }from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -21,18 +21,34 @@ const HelpPage = () => (
 );
 
 const NotFoundPage = () => (
-  <div>Page Not Found</div>
+  <div>
+  <p>Page Not Found</p>
+  <Link to='/'>Go back home</Link>
+  </div>
 );
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
+    <NavLink to='/create' activeClassName='is-active' exact={true}>Create new expense</NavLink>
+    <NavLink to='/edit' activeClassName='is-active' exact={true}>Edit expense</NavLink>
+    <NavLink to='/help' activeClassName='is-active' exact={true}>Help!</NavLink>
+  </header>
+); 
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path='/' exact={true} component={ExpenseDashboardPage} />
-      <Route path='/create' component={AddExpensePage} />
-      <Route path='/edit' component={EditExpensePage} />
-      <Route path='/help' component={HelpPage} /> 
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path='/' exact={true} component={ExpenseDashboardPage} />
+        <Route path='/create' component={AddExpensePage} />
+        <Route path='/edit' component={EditExpensePage} />
+        <Route path='/help' component={HelpPage} /> 
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
