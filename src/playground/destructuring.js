14224e513d507811cc9391646b56b2af9056db4d
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-24 18:24:26 +0100

    Section 10, Lecture 89

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -32,3 +32,15 @@ const book = {
 
 const {name: publisherName = 'Unknown'} = book.publisher;
 console.log(publisherName);
+
+// Array 
+
+const address = []; // [ '1299 Saint Patrick Street', 'Philadelphia', 'Pensylvania', '19147'];
+const [, , state = 'NY'] = address;
+console.log(`You are in ${state}`);
+
+// Challenge
+
+const item = ['coffee (hot)', '$2.00', '$2.50', '$3.00'];
+const [product,,medium] = item;
+console.log(`A ${product} is ${medium}`);
