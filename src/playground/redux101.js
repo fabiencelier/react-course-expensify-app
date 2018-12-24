06f9387b420c9fd80f13e8ab017aa86eaa79b9b8
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-24 18:47:29 +0100

    Section 10, Lecture 91

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -1,31 +1,30 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
-  
-switch(action.type) {
-  case 'INCREMENT':
-    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-    return {
-      count: state.count + incrementBy,
-    };
-  case 'DECREMENT':
-    const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
-    return {
-      count: state.count - decrementBy,
-    };
-  case 'RESET':
-    return {
-      count: 0,
-    };
-  case 'SET': {
-    return {
-      count: action.count,
+const reducer = (state = { count: 0 }, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.incrementBy,
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy,
+      };
+    case 'RESET':
+      return {
+        count: 0,
+      };
+    case 'SET': {
+      return {
+        count: action.count,
+      }
     }
-  }
-  default:
-    return state;
+    default:
+      return state;
   };
-});
+}
+
+const store = createStore(reducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
