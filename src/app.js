da6b80fa9be225185a5e332fdfbc0609b807ed31
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-25 20:13:39 +0100

    Section 11, Lecture 99

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
+import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
+
+import { addExpense } from './actions/expenses';
+import { setText as setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selecters/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'water bill', amount: '10000'}));
+store.dispatch(addExpense({description: 'rent', amount: '800000'}));
+store.dispatch(setTextFilter({ text: 'e'}));
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
