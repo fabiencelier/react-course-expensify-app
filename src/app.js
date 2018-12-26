a41da491b98d4922e7b8ca4d35b39379eeba17ce
fabiencelier <fabien.celier@polytechnique.edu>
2018-12-27 00:28:42 +0100

    Section 11, Lecture 104

--- src/app.js
+++ src/app.js
@@ -5,22 +5,13 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
-
 import { addExpense } from './actions/expenses';
-import { setText as setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selecters/expenses';
 
 const store = configureStore();
 
-setTimeout(() => {
-  store.dispatch(setTextFilter({ text: 'rent'}));
-
-}, 3000)
-
-store.dispatch(addExpense({description: 'water bill', amount: '10000'}));
-store.dispatch(addExpense({description: 'rent', amount: '800000'}));
-store.dispatch(setTextFilter({ text: 'e'}));
-console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+store.dispatch(addExpense({description: 'water bill', amount: 10000}));
+store.dispatch(addExpense({description: 'gaz bill', amount: 990000, createdAt: 1000}));
+store.dispatch(addExpense({description: 'rent', amount: 800000}));
 
 const jsx = (
   <Provider store={store}>
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setText as setTextFilter } from '../actions/filters';
+import { setText as setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
-    <input type='text' value={props.filters.text} onChange={(i) => {
-      props.dispatch(setTextFilter({ text: i.target.value}));
-    }}/>
+    <input
+      type='text'
+      value={props.filters.text} onChange={(i) => {
+        props.dispatch(setTextFilter({ text: i.target.value}));
+      }}/>
+    <select
+      value={props.filters.sortBy}
+      onChange={(i) => {
+        if(i.target.value === 'date') {
+          props.dispatch(sortByDate());
+        } else if (i.target.value === 'amount') {
+          props.dispatch(sortByAmount());
+        }
+      }}>
+      <option value='date'>Date</option>
+      <option value='amount'>Amount</option>
+    </select>
   </div>
 );
 
